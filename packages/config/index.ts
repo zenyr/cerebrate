4ae87a98ccbb3e1b1735003617eb35d82cff5b30
Jinhyeok Lee <zenyr@zenyr.com>
2025-10-02 10:36:08 +0900

    refactor(config): migrate to TypeScript

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,3 +1,3 @@
 // Export configurations
-export { default as eslintConfig } from './eslint.config.js';
+export { default as eslintConfig } from './eslint.config.ts';
 export { default as tsconfig } from './tsconfig.json';
